ccf619a35118f6ef561e3695eb300bb96b0b667e
alinerr <thxaline0902@outlook.com>
2020-03-27 12:05:58 +0800

    1. Prototype scene done. 2. Basic enemy & player movement. 3. Enemy will only chase target and abandon chasing for now.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] [Range(1, 20)] private float patrolDist = 10f;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (WithinDistance())
+        {
+            ChaseTarget();
+        }
+        else
+        {
+            AbandonChase();
+        }
+    }
+
+    private bool WithinDistance()
+    {
+        float sqrDist = Vector3.SqrMagnitude(target.position - transform.position);
+        return sqrDist <= patrolDist * patrolDist;
+    }
+    
+    //todo: for now, just chase
+    private void ChaseTarget()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    //todo: for now, just stop at where it is
+    private void AbandonChase()
+    {
+        agent.SetDestination(transform.position);
+    }
+
+
+#if UNITY_EDITOR
+    //todo: for debug use, remove later 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, patrolDist);
+    }
+#endif
+}
+
